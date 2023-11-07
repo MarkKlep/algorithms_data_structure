a0db90e59ee36ca21fe8af602d38e0c7f41206af
mark <cheapshoterr@gmail.com>
2023-11-07 20:00:11 +0200

    double linked list

---
+++ linked_list/linked_list/linked_list.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+template<typename T>
+struct Node {
+    T data;
+    Node<T>* prev;
+    Node<T>* next;
+
+    Node(T _data = T()) {
+        data = _data;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+template<typename T>
+class LinkedList {
+
+    Node<T>* head;
+    Node<T>* tail;
+
+public:
+    LinkedList() {
+        head = tail = nullptr;
+    }
+
+    ~LinkedList() {
+        Node<T>* curr = head;
+        while (curr) {
+            Node<T>* temp = curr;
+            curr = curr->next;
+            temp->prev = nullptr;
+            delete temp;
+        }
+        head = tail = nullptr;
+    }
+
+    void clear() {
+        Node<T>* curr = head;
+        while (curr) {
+            Node<T>* temp = curr;
+            curr = curr->next;
+            temp->prev = nullptr;
+            delete temp;
+        }
+        head = tail = nullptr;
+    }
+
+    void push_back(T data) {
+        if (head == nullptr) {
+            head = tail = new Node<T>(data);
+            return;
+        }
+
+        Node<T>* node = new Node<T>(data);
+
+        tail->next = node;
+        node->prev = tail;
+
+        tail = node;
+    }
+
+    void push_front(T data) {
+        if (head == nullptr) {
+            head = tail = new Node<T>(data);
+            return;
+        }
+
+        Node<T>* node = new Node<T>(data);
+
+        head->prev = node;
+        node->next = head;
+
+        head = node;
+    }
+
+    void pop_front() {
+        if (head == nullptr) {
+            return;
+        }
+
+        if (head == tail) {
+            Node<T>* temp = head;
+            head = tail = nullptr;
+            delete temp;
+
+            return;
+        }
+
+        Node<T>* temp = head;
+
+        head = head->next;
+        head->prev = nullptr;
+
+        temp->next = nullptr;
+        delete temp;
+    }
+
+    void pop_back() {
+        if (head == nullptr) {
+            return;
+        }
+
+        if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+            return;
+        }
+
+        Node<T>* temp = tail;
+
+        tail = tail->prev;
+        tail->next = nullptr;
+
+        temp->prev = nullptr;
+        delete temp;
+    }
+
+    void display() {
+        Node<T>* curr = head;
+
+        while (curr) {
+            cout << curr->data << " --> ";
+            curr = curr->next;
+        }
+        cout << endl;
+    }
+
+    void insert(const unsigned index, T data) {
+
+    }
+
+};
+
+int main() {
+    
+    LinkedList<int> _list;
+
+    _list.push_back(1);
+    _list.push_back(7);
+    _list.push_back(4);
+    _list.push_back(2);
+
+    _list.push_front(5);
+
+    _list.pop_back();
+
+    _list.pop_front();
+
+    _list.display();
+    
+    //list<int> l;
+  
+    return 0;
+}
---
+++ practise_algorithm/project01/project01.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+
+            }
+
+        }
+
+        return res;
+    }
+
+    bool isPalindrome(int x) {
+
+        if (x < 0)
+            return false;
+
+        int init_value = x;
+        int reversed = 0;
+
+        while (x) {
+            int digit = x % 10;
+
+            reversed = digit + reversed * 10;
+
+            x /= 10;
+        }
+
+        return reversed == init_value;
+    }
+
+    int romanToInt(string s) {
+
+        unordered_map<char, int> map;
+
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
+
+        int year = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s[i]] < map[s[i + 1]]) {
+                year -= map[s[i]];
+            }
+            else {
+                year += map[s[i]];
+            }
+        }
+
+        return year;
+    }
+};
+
+int main() {
+
+    Solution sol;
+
+    cout << sol.romanToInt("MMIV");
+
+	return 0;
+}
